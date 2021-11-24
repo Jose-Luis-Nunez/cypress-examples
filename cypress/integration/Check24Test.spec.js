b3075572122817e58b03f0203516ffbaac3c1895
Jose-Luis-Nunez <josenunez@outlook.de>
2021-11-24 21:55:50 +0100

    update tests and GHA yaml

--- cypress/integration/Check24Test.spec.js
+++ cypress/integration/Check24Test.spec.js
@@ -3,7 +3,7 @@ import {ResultPage} from "../pageobjects/ResultPage";
 import {TestUser} from "../utils/TestUser";
 import {RegistrationPage} from "../pageobjects/RegistrationPage";
 
-describe('product search test', () => {
+describe('Chek 24 tests', () => {
     const user = new TestUser()
     const startPage = new StartPage()
     const resultPage = new ResultPage()
@@ -20,22 +20,7 @@ describe('product search test', () => {
         expect(resultPage.getResultPageHeader()).to.exist
     })
 
-    it('should start login', () => {
-        startPage.acceptCookies()
-        startPage.clickOnLogin()
-        registration.createNewAccount(user)
-
-        expect(registration.getBackButton()).to.exist
-    })
-
-    it('should search for product2', () => {
-        startPage.acceptCookies()
-        startPage.searchForProduct("PS5")
-
-        expect(resultPage.getResultPageHeader()).to.exist
-    })
-
-    it('should start login2', () => {
+    it('should start registration', () => {
         startPage.acceptCookies()
         startPage.clickOnLogin()
         registration.createNewAccount(user)
