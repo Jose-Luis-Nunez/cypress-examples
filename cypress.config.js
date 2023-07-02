5852417351e9c8bf052495d85198f689b1c1113e
hey-jose-luis <josenunezheycar@gmail.com>
2023-07-02 09:45:10 +0200

    update: ci

--- cypress.config.js
+++ cypress.config.js
@@ -1,23 +1,26 @@
-const {defineConfig} = require('cypress')
+const {defineConfig} = require('cypress');
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const addCucumberPreprocessorPlugin =
-    require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
-const createEsbuildPlugin =
-    require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 
 module.exports = defineConfig({
     projectId: 'bikke8',
-    e2e: {
-        async setupNodeEvents(on, config) {
-            const bundler = createBundler({
-                plugins: [createEsbuildPlugin(config)],
-            });
-
-            on("file:preprocessor", bundler);
-            await addCucumberPreprocessorPlugin(on, config);
-
-            return config;
-        },
-        specPattern: ['cypress/e2e/features/*.feature', 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}']
+    // Use production build of Cypress
+    productionBrowserBuild: true,
+    // Specify the necessary spec files only
+    testFiles: [
+        'cypress/e2e/features/*.feature',
+        'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    ],
+    pluginsFile: 'cypress/plugins/index.js',
+    // Optimize preprocessors and plugins
+    on: async (config) => {
+        const bundler = createBundler({
+            plugins: [createEsbuildPlugin(config)],
+        });
+        config.env = config.env || {};
+        config.env.cucumber_preprocessor = { bundler };
+        await addCucumberPreprocessorPlugin(config);
+        return config;
     },
-})
+});
