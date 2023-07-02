a75f835e089d6072a766905cb68005b37364bc87
hey-jose-luis <josenunezheycar@gmail.com>
2023-07-02 09:49:55 +0200

    update: cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,26 +1,25 @@
-const {defineConfig} = require('cypress');
-const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
-const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const { defineConfig } = require('cypress');
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+const addCucumberPreprocessorPlugin = require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin;
+const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin;
 
 module.exports = defineConfig({
     projectId: 'bikke8',
-    // Use production build of Cypress
-    productionBrowserBuild: true,
-    // Specify the necessary spec files only
-    testFiles: [
-        'cypress/e2e/features/*.feature',
-        'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    ],
-    pluginsFile: 'cypress/plugins/index.js',
-    // Optimize preprocessors and plugins
-    on: async (config) => {
-        const bundler = createBundler({
-            plugins: [createEsbuildPlugin(config)],
-        });
-        config.env = config.env || {};
-        config.env.cucumber_preprocessor = { bundler };
-        await addCucumberPreprocessorPlugin(config);
-        return config;
+    // ... other configurations ...
+
+    // Configure the "e2e" testing type
+    e2e: {
+        // Configure setupNodeEvents
+        async setupNodeEvents(on, config) {
+            const bundler = createBundler({
+                plugins: [createEsbuildPlugin(config)],
+            });
+
+            on('file:preprocessor', bundler);
+            await addCucumberPreprocessorPlugin(on, config);
+
+            return config;
+        },
+        specPattern: ['cypress/e2e/features/*.feature', 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'],
     },
 });
