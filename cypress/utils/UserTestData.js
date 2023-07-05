fabd645137f434e681db66126749093924faf4c6
hey-jose-luis <josenunezheycar@gmail.com>
2023-07-05 13:02:37 +0200

    update: uninstall dependencies and update user test data

--- cypress/utils/UserTestData.js
+++ cypress/utils/UserTestData.js
@@ -1,34 +1,26 @@
-import { uniqueNamesGenerator, colors, starWars, NumberDictionary } from 'unique-names-generator';
-
 export class UserTestData {
-
     constructor() {
         this.email = this.getRandomEmail();
         this.password = this.getRandomPassword();
     }
 
-    getRandomEmail() {
-        const randomNumber = this.getRandomNumber(7);
-        const characterName = uniqueNamesGenerator({
-            dictionaries: [starWars, colors, randomNumber],
-        })
-        return `${characterName}@gmail.com`;
-    }
-
     getRandomPassword() {
-        return `pw${this.getRandomNumber(6)}`;
+        return `pw${ this.getCurrentTime() }`;
     }
 
-    getRandomNumber(numberOfDigits) {
-        let minRange = 1;
-        let maxRange = 9;
+    getRandomPhoneNumber() {
+        return `${ this.getCurrentTime() }${this.getRandomDigit(9)}`;
+    }
 
-        for (let i = 0; i < numberOfDigits; i++) {
-            if (i > 0) {
-                minRange = minRange * 10;
-                maxRange = maxRange * 10;
-            }
-        }
-        return NumberDictionary.generate({min: minRange, max: maxRange});
+    getRandomEmail() {
+        return `josenunezumi+${this.getCurrentTime()}${this.getRandomDigit(999)}@gmail.rofl`;
+    }
+
+    getCurrentTime() {
+        return Date.now();
+    }
+
+    getRandomDigit(max) {
+        return Math.floor(Math.random() * (max + 1));
     }
 }
