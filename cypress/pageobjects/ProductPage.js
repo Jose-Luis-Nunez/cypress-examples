a6f147d26dcd4c26a1598a94d1da5caa5af8f740
hey-jose-luis <josenunezheycar@gmail.com>
2023-07-10 21:27:18 +0200

    update: selectFirstProduct

--- cypress/pageobjects/ProductPage.js
+++ cypress/pageobjects/ProductPage.js
@@ -11,7 +11,7 @@ export class ProductPage extends AbstractPage {
     }
 
     selectFirstProduct() {
-        cy.get(this.productTiles).should('be.visible').first().click({ force: true });
+        cy.get(this.productTiles).first().click({ force: true });
     }
 
     selectManufacturer(manufacturer) {
